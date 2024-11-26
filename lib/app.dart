3d507b207a09d47b54fe7aec75ab0b5e62bfaf1d
bibhakta21 <bibhaktalamsal8@gmail.com>
2024-11-26 15:30:34 +0545

    Add main app structure with DashboardView as the home page

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,16 @@
-
-import 'package:first_flutter_project/view/arithmetic_view.dart';
+import 'package:first_flutter_project/view/dashboard_view.dart';
 import 'package:flutter/material.dart';
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ArithmeticView(),
+    return MaterialApp(
+      title: 'Flutter Dashboard',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const DashboardView(),
     );
   }
 }
